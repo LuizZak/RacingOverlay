2abfb8a826d35a93e6cf9c932ba1309909b12f96
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2025-06-20 09:36:01 -0300

    Implementing lock icon on traction overlay when wheels are detected to be locked up

--- scripts/wheel_metrics.gd
+++ scripts/wheel_metrics.gd
@@ -29,32 +29,19 @@ func _ready() -> void:
     reset()
 
 func reset() -> void:
-    wheel_entries[Wheel.REAR_LEFT].fill_color = _color_for_wheel(0.0, 0.0)
-    wheel_entries[Wheel.REAR_RIGHT].fill_color = _color_for_wheel(0.0, 0.0)
-    wheel_entries[Wheel.FRONT_LEFT].fill_color = _color_for_wheel(0.0, 0.0)
-    wheel_entries[Wheel.FRONT_RIGHT].fill_color = _color_for_wheel(0.0, 0.0)
+    wheel_entries[Wheel.REAR_LEFT].update(0.0, 0.0)
+    wheel_entries[Wheel.REAR_RIGHT].update(0.0, 0.0)
+    wheel_entries[Wheel.FRONT_LEFT].update(0.0, 0.0)
+    wheel_entries[Wheel.FRONT_RIGHT].update(0.0, 0.0)
 
 func update_with_packet(packet: GamePacketBase) -> void:
     queue_redraw()
 
     if packet is Dirt2GamePacket:
-        wheel_entries[Wheel.REAR_LEFT].fill_color = _color_for_wheel(packet.speed_ms, packet.wsp_rl)
-        wheel_entries[Wheel.REAR_RIGHT].fill_color = _color_for_wheel(packet.speed_ms, packet.wsp_rr)
-        wheel_entries[Wheel.FRONT_LEFT].fill_color = _color_for_wheel(packet.speed_ms, packet.wsp_fl)
-        wheel_entries[Wheel.FRONT_RIGHT].fill_color = _color_for_wheel(packet.speed_ms, packet.wsp_fr)
-
-func _color_for_wheel(vehicle_speed: float, wheel_speed: float) -> Color:
-    if vehicle_speed == wheel_speed:
-        return Color.GREEN
-
-    var tolerance := maxf(0.1, abs(vehicle_speed) / 2.0)
-    var diff := absf(vehicle_speed - wheel_speed)
-    var ratio = minf(1.0, diff / tolerance)
-
-    if wheel_speed > vehicle_speed:
-        return Color.GREEN.lerp(Color.YELLOW, ratio)
-
-    return Color.GREEN.lerp(Color.RED, ratio)
+        wheel_entries[Wheel.REAR_LEFT].update(packet.speed_ms, packet.wsp_rl)
+        wheel_entries[Wheel.REAR_RIGHT].update(packet.speed_ms, packet.wsp_rr)
+        wheel_entries[Wheel.FRONT_LEFT].update(packet.speed_ms, packet.wsp_fl)
+        wheel_entries[Wheel.FRONT_RIGHT].update(packet.speed_ms, packet.wsp_fr)
 
 func _draw() -> void:
     # Draw background
@@ -95,14 +82,82 @@ class WheelEntry:
     var position: Vector2
     var size: Vector2
     var fill_color: Color = Color.TRANSPARENT
+    var is_locked: bool = false
 
-    func _init(position: Vector2, size: Vector2) -> void:
+    func _init(position: Vector2, size: Vector2 = WHEEL_SIZE) -> void:
         self.position = position
         self.size = size
+
+    func update(vehicle_speed: float, wheel_speed: float) -> void:
+        if vehicle_speed == wheel_speed:
+            fill_color = Color.GREEN
+            is_locked = false
+            return
+
+        var tolerance := maxf(0.1, abs(vehicle_speed) / 2.0)
+        var diff := absf(vehicle_speed - wheel_speed)
+        var ratio := minf(1.0, diff / tolerance)
+
+        if wheel_speed > vehicle_speed:
+            fill_color = Color.GREEN.lerp(Color.YELLOW, ratio)
+        else:
+            fill_color = Color.GREEN.lerp(Color.RED, ratio)
+
+        var lock_tolerance := 0.1
+
+        if absf(vehicle_speed) > lock_tolerance and absf(wheel_speed) < lock_tolerance:
+            is_locked = true
+        else:
+            is_locked = false
 
     func draw(canvas_item: CanvasItem) -> void:
         canvas_item.draw_rect(bounds(), fill_color, true)
         canvas_item.draw_rect(bounds(), Color.BLACK, false, 1, true)
+
+        if is_locked:
+            draw_padlock(canvas_item)
+
+    func draw_padlock(canvas_item: CanvasItem) -> void:
+        var width := 0.7
+
+        # Draw padlock body
+        var padlock_body_bounds := bounds()
+        padlock_body_bounds = padlock_body_bounds.grow(-3)
+        padlock_body_bounds.size.y = padlock_body_bounds.size.x
+        padlock_body_bounds.position.y = self.get_center_left().y - padlock_body_bounds.size.y * 0.3
+
+        canvas_item.draw_rect(padlock_body_bounds, Color.BLACK, false, width, true)
+
+        # Draw padlock keyhole
+        var keyhole_start := padlock_body_bounds.get_center()
+        var keyhole_end := keyhole_start + padlock_body_bounds.size * Vector2(0, 0.2)
+        canvas_item.draw_line(
+            keyhole_start, keyhole_end, Color.BLACK, width, true
+        )
+
+        # Draw padlock shackle
+        var shackle_arc_radius := padlock_body_bounds.size.x * 0.4
+        var shackle_arc_center := Vector2(
+            padlock_body_bounds.get_center().x,
+            padlock_body_bounds.position.y - shackle_arc_radius * 0.6
+        )
+        var shackle_left := Vector2(
+            shackle_arc_center.x - shackle_arc_radius,
+            shackle_arc_center.y
+        )
+        var shackle_right := Vector2(
+            shackle_arc_center.x + shackle_arc_radius,
+            shackle_arc_center.y
+        )
+        canvas_item.draw_arc(
+            shackle_arc_center, shackle_arc_radius, -PI, 0, 10, Color.BLACK, width, true
+        )
+        canvas_item.draw_line(
+            shackle_left, Vector2(shackle_left.x, padlock_body_bounds.position.y), Color.BLACK, width, true
+        )
+        canvas_item.draw_line(
+            shackle_right, Vector2(shackle_right.x, padlock_body_bounds.position.y), Color.BLACK, width, true
+        )
 
     func bounds() -> Rect2:
         return Rect2(position - size / 2, size)
@@ -114,21 +169,13 @@ class WheelEntry:
         return position + size * Vector2(0.5, 0.0)
 
     static func make_rear_left() -> WheelEntry:
-        return WheelEntry.new(
-            WHEEL_OFFSET * Vector2(-1, 1), WHEEL_SIZE
-        )
+        return WheelEntry.new(WHEEL_OFFSET * Vector2(-1, 1))
 
     static func make_rear_right() -> WheelEntry:
-        return WheelEntry.new(
-            WHEEL_OFFSET * Vector2(1, 1), WHEEL_SIZE
-        )
+        return WheelEntry.new(WHEEL_OFFSET * Vector2(1, 1))
 
     static func make_front_left() -> WheelEntry:
-        return WheelEntry.new(
-            WHEEL_OFFSET * Vector2(-1, -1), WHEEL_SIZE
-        )
+        return WheelEntry.new(WHEEL_OFFSET * Vector2(-1, -1))
 
     static func make_front_right() -> WheelEntry:
-        return WheelEntry.new(
-            WHEEL_OFFSET * Vector2(1, -1), WHEEL_SIZE
-        )
+        return WheelEntry.new(WHEEL_OFFSET * Vector2(1, -1))
